5bf89bbf64245130805b8aa1be47d380c27b280b
Kyler2424 <ksimmonsayala@sfsu.edu>
2024-03-17 23:45:11 -0700

    fdajskdf

--- resources/js-question.js
+++ resources/js-question.js
@@ -1,3 +1,4 @@
-window.addEventListener("load", () => {
-    console.log("This function is executed once the page is fully loaded");
-  });
+// window.addEventListener("load", () => {
+//     console.log("This function is executed once the page is fully loaded");
+//   });
+document.getElementById("output").innerHTML = "Hello, world!";
